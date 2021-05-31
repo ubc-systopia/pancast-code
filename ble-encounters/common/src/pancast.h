20a462d4f8bc87ec344c8a30a208d4c8ffa95a09
rlnsy <rowlindsay@gmail.com>
2021-05-31 13:46:16 -0700

    test data

--- ble-encounters/common/src/pancast.h
+++ ble-encounters/common/src/pancast.h
@@ -1,16 +1,17 @@
+#ifndef PANCAST__H
+#define PANCAST__H
+
 #include <stdint.h>
 
 #define CONFIG_BT_EXT_ADV 0 // set to 1 to allow extended advertising
 
-#define BEACON_SK_SIZE 512 // 4096 bits
+#define BEACON_SK_SIZE 16 // bytes
 
 typedef struct {
 	uint8_t bytes[BEACON_SK_SIZE];
 } beacon_sk_t;
 
 // hard-coded beacon information for development purposes
-#define BEACON_ID		0x012345678
-#define BEACON_LOC_ID	0x0123456789abcdef
 
 #define BEACON_EPOCH_LENGTH			15	 // number of timer cycles in one epoch should correspond to 15 min in prod.
 #define BEACON_TIMER_RESOLUTION		1000 // in ms TODO: should be 1 min in prod.
@@ -55,3 +56,5 @@ typedef struct {
 #define epoch_i(t, C) (beacon_epoch_counter_t)((t - C)/BEACON_EPOCH_LENGTH)
 
 #define DONGLE_ENCOUNTER_MIN_TIME 5 // number of time units eph. id must be observed
+
+#endif

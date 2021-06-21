86a93d791778171d1a82206fd21281ab1cd67a13
rlnsy <rowlindsay@gmail.com>
2021-06-21 14:09:24 -0700

    terminal emulator

--- ble-encounters/terminal/src/main.c
+++ ble-encounters/terminal/src/main.c
@@ -12,10 +12,13 @@
 #include <bluetooth/gatt.h>
 #include <sys/byteorder.h>
 
-#define LOG_LEVEL__DEBUG
+#define LOG_LEVEL__INFO
 #include "../../common/src/log.h"
 #include "../../dongle/src/dongle.h"
 #include "../../common/src/util.h"
+#include "../../dongle/src/test.h"
+#include "../../dongle/src/upload.h"
+#include "../../dongle/src/encounter.h"
 
 static void start_scan(void);
 
@@ -28,6 +31,20 @@ static struct bt_gatt_subscribe_params subscribe_params;
 
 struct k_mutex state_mu;
 interact_state state;
+
+enctr_entry_counter_t num_recs;
+dongle_encounter_entry entry;
+enctr_entry_counter_t num_received;
+
+#define TERMINAL_UPLOAD_STATE_UNCONNECTED 0x00
+#define TERMINAL_UPLOAD_STATE_LOCKED 0x01
+#define TERMINAL_UPLOAD_STATE_RECEIVE_DATA_0 0x02
+#define TERMINAL_UPLOAD_STATE_RECEIVE_DATA_1 0x03
+#define TERMINAL_UPLOAD_STATE_RECEIVE_DATA_2 0x04
+#define TERMINAL_UPLOAD_STATE_RECEIVE_DATA_3 0x05
+#define TERMINAL_UPLOAD_STATE_RECEIVE_DATA_4 0x06
+
+uint8_t terminal_state = TERMINAL_UPLOAD_STATE_UNCONNECTED;
 
 BT_GATT_SERVICE_DEFINE(service,
                        // 0. Primary Service Attribute
@@ -45,36 +62,130 @@ static uint8_t notify_func(struct bt_conn *conn,
 {
     if (!data)
     {
-        printk("[UNSUBSCRIBED]\n");
+        log_info("[UNSUBSCRIBED]\n");
         params->value_handle = 0U;
         return BT_GATT_ITER_STOP;
     }
 
-    printk("[NOTIFICATION] data length %u\n", length);
-    info_bytes(data, length, "Data");
+    log_debugf("[NOTIFICATION] data length %u\n", length);
+    print_bytes(data, length, "Data");
 
     k_mutex_lock(&state_mu, K_FOREVER);
-
     memcpy(&state, data, sizeof(interact_state));
-
-    int locked = state.flags & 0b00000001 >> 0;
-
-    printk("Dongle is %s\n", locked ? "locked" : "unlocked");
-
-    if (locked)
-    {
-        state.flags = 0b00000000;
-    }
-    else
-    {
-        state.flags = 0b00000001;
-    }
-
-    bt_gatt_notify(NULL, &service.attrs[1], &state, sizeof(interact_state));
-
     k_mutex_unlock(&state_mu);
 
+    interact_update();
+
     return BT_GATT_ITER_CONTINUE;
+}
+
+void interact_update()
+{
+    k_mutex_lock(&state_mu, K_FOREVER);
+    switch (terminal_state)
+    {
+    case TERMINAL_UPLOAD_STATE_LOCKED:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_NUM_RECS)
+        {
+            break;
+        }
+        num_recs = *((dongle_epoch_counter_t *)state.num_recs.val);
+        log_infof("Number of records: %llu\n", num_recs);
+        if (num_recs == 0)
+        {
+            terminal_state = TERMINAL_UPLOAD_STATE_LOCKED;
+            break;
+        }
+        log_info("Fetching records...\n");
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_0;
+        num_received = 0;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_NUM_RECS;
+        peer_update();
+        break;
+    case TERMINAL_UPLOAD_STATE_RECEIVE_DATA_0:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_DATA_0)
+        {
+            break;
+        }
+        log_debug("Receiving data 0\n");
+        memcpy(&entry.beacon_id, state.data.bytes, sizeof(beacon_id_t));
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_1;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_DATA_0;
+        peer_update();
+        break;
+    case TERMINAL_UPLOAD_STATE_RECEIVE_DATA_1:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_DATA_1)
+        {
+            break;
+        }
+        log_debug("Receiving data 1\n");
+        memcpy(&entry.beacon_time, state.data.bytes, sizeof(beacon_timer_t));
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_2;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_DATA_1;
+        peer_update();
+        break;
+    case TERMINAL_UPLOAD_STATE_RECEIVE_DATA_2:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_DATA_2)
+        {
+            break;
+        }
+        log_debug("Receiving data 2\n");
+        memcpy(&entry.dongle_time, state.data.bytes, sizeof(dongle_timer_t));
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_3;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_DATA_2;
+        peer_update();
+        break;
+    case TERMINAL_UPLOAD_STATE_RECEIVE_DATA_3:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_DATA_3)
+        {
+            break;
+        }
+        log_debug("Receiving data 3\n");
+        memcpy(entry.eph_id.bytes, state.data.bytes, BEACON_EPH_ID_SIZE);
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_4;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_DATA_3;
+        peer_update();
+        break;
+    case TERMINAL_UPLOAD_STATE_RECEIVE_DATA_4:
+        if (state.flags != DONGLE_UPLOAD_DATA_TYPE_DATA_4)
+        {
+            break;
+        }
+        log_debug("Receiving data 4\n");
+        memcpy(&entry.location_id, state.data.bytes, sizeof(beacon_location_id_t));
+        num_received++;
+        if (num_received == num_recs)
+        {
+            log_info("All records received\n");
+        }
+        // else
+        // {
+        //     log_errorf("Records count mismatch! (Expected: %llu, Recvd: %llu)\n", num_recs, num_received);
+        // }
+        terminal_state = TERMINAL_UPLOAD_STATE_RECEIVE_DATA_0;
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_ACK_DATA_4;
+        peer_update();
+        //_display_encounter_(&entry);
+        break;
+    default:
+        log_errorf("No match for state! (%d)\n", terminal_state);
+    }
+    k_mutex_unlock(&state_mu);
+}
+
+void peer_update()
+{
+    k_mutex_lock(&state_mu, K_FOREVER);
+    bt_gatt_notify(NULL, &service.attrs[1], &state, sizeof(interact_state));
+    k_mutex_unlock(&state_mu);
+}
+
+int state_eql(uint8_t state)
+{
+    k_mutex_lock(&state_mu, K_FOREVER);
+    int val = (terminal_state == state);
+    k_mutex_unlock(&state_mu);
+    return val;
 }
 
 static uint8_t discover_func(struct bt_conn *conn,
@@ -85,12 +196,12 @@ static uint8_t discover_func(struct bt_conn *conn,
 
     if (!attr)
     {
-        printk("Discover complete\n");
+        log_info("Discover complete\n");
         (void)memset(params, 0, sizeof(*params));
         return BT_GATT_ITER_STOP;
     }
 
-    printk("[ATTRIBUTE] handle %u\n", attr->handle);
+    log_debugf("[ATTRIBUTE] handle %u\n", attr->handle);
 
     if (!bt_uuid_cmp(discover_params.uuid, BT_UUID_DECLARE_128(DONGLE_SERVICE_UUID)))
     {
@@ -102,7 +213,7 @@ static uint8_t discover_func(struct bt_conn *conn,
         err = bt_gatt_discover(conn, &discover_params);
         if (err)
         {
-            printk("Characteristic Discover failed (err %d)\n", err);
+            log_errorf("Characteristic Discover failed (err %d)\n", err);
         }
     }
     else if (!bt_uuid_cmp(discover_params.uuid,
@@ -117,7 +228,7 @@ static uint8_t discover_func(struct bt_conn *conn,
         err = bt_gatt_discover(conn, &discover_params);
         if (err)
         {
-            printk("CCC Discover failed (err %d)\n", err);
+            log_errorf("CCC Discover failed (err %d)\n", err);
         }
     }
     else
@@ -129,11 +240,14 @@ static uint8_t discover_func(struct bt_conn *conn,
         err = bt_gatt_subscribe(conn, &subscribe_params);
         if (err && err != -EALREADY)
         {
-            printk("Subscribe failed (err %d)\n", err);
+            log_errorf("Subscribe failed (err %d)\n", err);
         }
         else
         {
-            printk("[SUBSCRIBED]\n");
+            log_info("[SUBSCRIBED]\n");
+            k_mutex_lock(&state_mu, K_FOREVER);
+            terminal_state = TERMINAL_UPLOAD_STATE_LOCKED;
+            k_mutex_unlock(&state_mu);
         }
 
         return BT_GATT_ITER_STOP;
@@ -150,7 +264,7 @@ void parse_data(uint8_t type, uint8_t *data, uint16_t len, bt_addr_le_t *addr)
     case BT_DATA_UUID128_ALL:
         if ((len * 8) != 128U)
         {
-            printk("AD malformed\n");
+            log_debug("AD malformed\n");
             return;
         }
 
@@ -174,12 +288,12 @@ void parse_data(uint8_t type, uint8_t *data, uint16_t len, bt_addr_le_t *addr)
             return;
         }
 
-        printk("Service ID match\n");
+        log_debugf("Service ID match\n");
 
         err = bt_le_scan_stop();
         if (err)
         {
-            printk("Stop LE scan failed (err %d)\n", err);
+            log_errorf("Stop LE scan failed (err %d)\n", err);
             return;
         }
 
@@ -188,13 +302,13 @@ void parse_data(uint8_t type, uint8_t *data, uint16_t len, bt_addr_le_t *addr)
                                 param, &default_conn);
         if (err)
         {
-            printk("Create conn failed (err %d)\n", err);
+            log_errorf("Create conn failed (err %d)\n", err);
             start_scan();
         }
 
         return;
     default:
-        printk("Data type is not 128 UUID\n");
+        log_debugf("Data type is not 128 UUID\n");
         return;
     }
 }
@@ -205,8 +319,8 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
     char dev[BT_ADDR_LE_STR_LEN];
 
     bt_addr_le_to_str(addr, dev, sizeof(dev));
-    printk("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
-           dev, type, ad->len, rssi);
+    log_debugf("[DEVICE]: %s, AD evt type %u, AD data len %u, RSSI %i\n",
+               dev, type, ad->len, rssi);
 
     if (strcmp(dev, "D5:AA:E4:57:78:50 (random)"))
     {
@@ -217,7 +331,7 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
     if (type == BT_GAP_ADV_TYPE_ADV_IND ||
         type == BT_GAP_ADV_TYPE_ADV_DIRECT_IND)
     {
-        info_bytes(ad->data, ad->len, "ad data");
+        print_bytes(ad->data, ad->len, "ad data");
         parse_data(ad->data[1], ad->data + 2, ad->data[0] - 1, addr);
         //bt_data_parse(ad, eir_found, (void *)addr);
     }
@@ -239,11 +353,11 @@ static void start_scan(void)
     err = bt_le_scan_start(&scan_param, device_found);
     if (err)
     {
-        printk("Scanning failed to start (err %d)\n", err);
+        log_errorf("Scanning failed to start (err %d)\n", err);
         return;
     }
 
-    printk("Scanning successfully started\n");
+    log_info("Scanning successfully started\n");
 }
 
 static void connected(struct bt_conn *conn, uint8_t conn_err)
@@ -255,7 +369,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
 
     if (conn_err)
     {
-        printk("Failed to connect to %s (%u)\n", addr, conn_err);
+        log_errorf("Failed to connect to %s (%u)\n", addr, conn_err);
 
         bt_conn_unref(default_conn);
         default_conn = NULL;
@@ -264,7 +378,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
         return;
     }
 
-    printk("Connected: %s\n", addr);
+    log_infof("Connected: %s\n", addr);
 
     if (conn == default_conn)
     {
@@ -278,7 +392,7 @@ static void connected(struct bt_conn *conn, uint8_t conn_err)
         err = bt_gatt_discover(default_conn, &discover_params);
         if (err)
         {
-            printk("Discover failed(err %d)\n", err);
+            log_errorf("Discover failed(err %d)\n", err);
             return;
         }
     }
@@ -290,7 +404,7 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
 
     bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
 
-    printk("Disconnected: %s (reason 0x%02x)\n", addr, reason);
+    log_infof("Disconnected: %s (reason 0x%02x)\n", addr, reason);
 
     if (default_conn != conn)
     {
@@ -300,12 +414,23 @@ static void disconnected(struct bt_conn *conn, uint8_t reason)
     bt_conn_unref(default_conn);
     default_conn = NULL;
 
+    k_mutex_lock(&state_mu, K_FOREVER);
+    terminal_state = TERMINAL_UPLOAD_STATE_UNCONNECTED;
+    k_mutex_unlock(&state_mu);
+
     start_scan();
 }
 
 static struct bt_conn_cb conn_callbacks = {
     .connected = connected,
     .disconnected = disconnected,
+};
+
+static dongle_otp_val bad_otps[4] = {
+    00000000,
+    00000001,
+    00000002,
+    00000003,
 };
 
 void main(void)
@@ -315,13 +440,33 @@ void main(void)
 
     if (err)
     {
-        printk("Bluetooth init failed (err %d)\n", err);
+        log_errorf("Bluetooth init failed (err %d)\n", err);
         return;
     }
 
-    printk("Bluetooth initialized\n");
+    log_info("Bluetooth initialized\n");
 
     bt_conn_cb_register(&conn_callbacks);
 
     start_scan();
+
+    while (state_eql(TERMINAL_UPLOAD_STATE_UNCONNECTED))
+    {
+        log_info("Not connected yet\n");
+        k_sleep(K_MSEC(1000));
+    }
+    log_debug("Connected and subscribed\n");
+    int i = 0;
+    while (state_eql(TERMINAL_UPLOAD_STATE_LOCKED))
+    {
+        log_info("Must enter code\n");
+        dongle_otp_val otp = i >= 4 ? TEST_OTPS[3].val : bad_otps[i];
+        k_mutex_lock(&state_mu, K_FOREVER);
+        state.flags = DONGLE_UPLOAD_DATA_TYPE_OTP;
+        memcpy(state.otp.val, &otp, sizeof(dongle_otp_val));
+        k_mutex_unlock(&state_mu);
+        peer_update();
+        i++;
+        k_sleep(K_MSEC(1000));
+    }
 }

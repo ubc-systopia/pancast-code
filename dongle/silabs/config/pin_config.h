be8022a346ed8d93e245b9e0e672927d5e77718c
rlnsy <rowlindsay@gmail.com>
2021-06-30 17:14:12 -0700

    force generation

--- dongle/silabs/config/pin_config.h
+++ dongle/silabs/config/pin_config.h
@@ -77,6 +77,22 @@
 // [USART0]$
 
 // $[USART1]
+// USART1 CTS on PA08
+#define USART1_CTS_PORT                          gpioPortA
+#define USART1_CTS_PIN                           8
+
+// USART1 RTS on PA07
+#define USART1_RTS_PORT                          gpioPortA
+#define USART1_RTS_PIN                           7
+
+// USART1 RX on PA06
+#define USART1_RX_PORT                           gpioPortA
+#define USART1_RX_PIN                            6
+
+// USART1 TX on PA05
+#define USART1_TX_PORT                           gpioPortA
+#define USART1_TX_PIN                            5
+
 // [USART1]$
 
 // $[I2C1]

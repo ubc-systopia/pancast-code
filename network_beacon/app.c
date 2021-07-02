02910d7598cb0366573527c0cc11c21f628a4860
Nichole Boufford <nboufford@dhcp-128-189-203-71.ubcsecure.wireless.ubc.ca>
2021-07-02 10:37:03 -0700

    use GPIO as signal instead of UART

--- network_beacon/app.c
+++ network_beacon/app.c
@@ -22,6 +22,7 @@
 #include "app_assert.h"
 #include "sl_bluetooth.h"
 #include "sl_iostream.h"
+#include "em_gpio.h"
 
 #include "src/beacon.h"
 
@@ -46,8 +47,10 @@ void app_init(void)
 {
     app_iostream_eusart_init();
     risk_data_len = RISK_DATA_SIZE;
-    adv_index = 0;
     memset(&risk_data, 0, risk_data_len);
+
+    // Set pin PB01 for output
+    GPIO_PinModeSet(gpioPortB, 1, gpioModePushPull, 0);
 }
 
 /* Update risk data after receive from raspberry pi client */
@@ -82,22 +85,27 @@ void update_risk_data(int len, char *data)
 void get_risk_data()
 {
 
-    uint8_t ready = XON;
+    // set ready pin
+    GPIO_PinOutSet(gpioPortB, 1);
 
-    // request next chunk of data
-    write(SL_IOSTREAM_STDOUT, &ready, sizeof(uint8_t));
-
-    int read_len;
+    int read_len = 0;
     char buf[PER_ADV_SIZE];
 
-    do
+    read_len = read(SL_IOSTREAM_STDIN, &buf, PER_ADV_SIZE);
+
+    // read until data returned
+    while (read_len < 0)
     {
         read_len = read(SL_IOSTREAM_STDIN, &buf, PER_ADV_SIZE);
-    } while (read_len > 0);
+    }
 
-    // Update broadcast data
+    // clear pin once data has been recieved
+    GPIO_PinOutClear(gpioPortB, 1);
+
+    // update broadcast data
     update_risk_data(PER_ADV_SIZE, buf);
-    free(buf);
+
+    // probably want to reset timer here
 }
 
 void sl_timer_on_expire(sl_sleeptimer_timer_handle_t *handle, void *data)
@@ -110,24 +118,6 @@ void sl_timer_on_expire(sl_sleeptimer_timer_handle_t *handle, void *data)
     }
 
     sl_status_t sc;
-
-    // handle periodic set advertising data
-    if (sec_clock % PER_UPDATE_FREQ == 0)
-    {
-        sc = sl_bt_advertiser_set_data(advertising_set_handle, 8, PER_ADV_SIZE,
-                                       &risk_data[adv_index * PER_ADV_SIZE]);
-        if (sc != 0)
-        {
-            printf("sc: %lx \r\n", sc);
-        }
-        app_assert_status(sc);
-
-        adv_index++;
-        if (adv_index * PER_ADV_SIZE > risk_data_len)
-        {
-            adv_index = 0;
-        }
-    }
 
     // handle data updates
     if (sec_clock % RISK_UPDATE_FREQ == 0)

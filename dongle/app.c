330818478f0ce9bc07233600f4792ed946d5d723
rlnsy <rowlindsay@gmail.com>
2021-07-14 10:38:04 -0700

    open sync

--- dongle/app.c
+++ dongle/app.c
@@ -19,11 +19,15 @@
 
 #include "sl_bluetooth.h"
 #include "app_log.h"
+#include "app_assert.h"
 
 #include "src/dongle.h"
 
 #define LOG_LEVEL__INFO
 #include "../../common/src/log.h"
+
+// Sync handle
+static uint16_t sync_handle = 0;
 
 
 void sl_timer_on_expire(sl_sleeptimer_timer_handle_t *handle, void *data)
@@ -50,6 +54,7 @@ void app_process_action(void)
 
 void sl_bt_on_event (sl_bt_msg_t *evt)
 {
+  sl_status_t sc;
   switch (SL_BT_MSG_ID(evt->header)) {
       case sl_bt_evt_system_boot_id:
         app_log_info("Bluetooth start\r\n");
@@ -58,8 +63,24 @@ void sl_bt_on_event (sl_bt_msg_t *evt)
         break;
       case sl_bt_evt_scanner_scan_report_id:
 #define report (evt->data.evt_scanner_scan_report)
+        // First, log into the legacy decode pipeline
         dongle_log(&report.address,
                    report.rssi, report.data.data, report.data.len);
+        // then check for periodic info in packet
+        if (evt->data.evt_scanner_scan_report.periodic_interval != 0) {
+//         // Start test
+//         sync_test.start_ticks = sl_sleeptimer_get_tick_count64();
+           app_log_info("Opening sync\r\n");
+           // Open sync
+           sc = sl_bt_sync_open(evt->data.evt_scanner_scan_report.address,
+                           evt->data.evt_scanner_scan_report.address_type,
+                           evt->data.evt_scanner_scan_report.adv_sid,
+                           &sync_handle);
+           if (sc != 0) {
+               app_log_info("sc: 0x%x\r\n", sc);
+           }
+           app_assert_status(sc);
+       }
 #undef report
         break;
       case sl_bt_evt_system_soft_timer_id:

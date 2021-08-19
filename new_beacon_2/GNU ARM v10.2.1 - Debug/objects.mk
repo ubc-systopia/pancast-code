699b6339bdc55a5c91dd8a83ff97f5ba66ee21cd
Rowan Lindsay <rowan@rowanlindsay.com>
2021-08-19 16:39:50 -0700

    remove new_beacon_2

--- new_beacon_2/GNU ARM v10.2.1 - Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.2//protocol/bluetooth/lib/EFR32MG22/GCC/libbluetooth.a /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.2//platform/emdrv/nvm3/lib/libnvm3_CM33_gcc.a /Applications/Simplicity\ Studio.app/Contents/Eclipse/developer/sdks/gecko_sdk_suite/v3.2//platform/radio/rail_lib/autogen/librail_release/librail_efr32xg22_gcc_release.a
-
-LIBS := -lgcc -lc -lm -lnosys
-

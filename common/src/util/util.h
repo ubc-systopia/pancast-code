666aa14d65772eb54bcbe3ffe7dd919dbf7f194e
Aastha Mehta <aasthakm@mpi-sws.org>
2021-11-22 16:04:45 -0800

    braces in macro code

--- common/src/util/util.h
+++ common/src/util/util.h
@@ -7,24 +7,26 @@
 
 #include <stdint.h>
 
-#define log_bytes(log, logf, data, len, name, arg1, arg2, arg3, arg4) \
-  uint32_t time_ms = (uint32_t) (sl_sleeptimer_get_tick_count64()     \
-      * 1000 / sl_sleeptimer_get_timer_frequency());                  \
-      \
-      logf(APP_LOG_TIME_FORMAT APP_LOG_SEPARATOR                      \
-          "%s: 0x ", (time_ms / 3600000),                             \
-          ((time_ms / 60000) % 60), ((time_ms / 1000) % 60),          \
-          (time_ms % 1000), name);                                    \
-      \
-  for (unsigned int i = 0; i < len; i++)                              \
-  {                                                                   \
-      if (i != 0 && !(i % 16))                                        \
-      {                                                               \
-          logf("%s", "\r\n");                                         \
-      }                                                               \
-      logf("%.2x ", ((uint8_t *)data)[i]);                            \
-  }                                                                   \
-  logf("%u %lu %u %d\r\n", arg1, arg2, arg3, arg4)
+#define log_bytes(log, logf, data, len, name, arg1, arg2, arg3, arg4)   \
+  {                                                                  \
+    uint32_t time_ms = (uint32_t) (sl_sleeptimer_get_tick_count64()     \
+        * 1000 / sl_sleeptimer_get_timer_frequency());                  \
+        \
+        logf(APP_LOG_TIME_FORMAT APP_LOG_SEPARATOR                      \
+            "%s: 0x ", (time_ms / 3600000),                             \
+            ((time_ms / 60000) % 60), ((time_ms / 1000) % 60),          \
+            (time_ms % 1000), name);                                    \
+        \
+    for (unsigned int i = 0; i < len; i++)                              \
+    {                                                                   \
+        if (i != 0 && !(i % 16))                                        \
+        {                                                               \
+            logf("%s", "\r\n");                                         \
+        }                                                               \
+        logf("%.2x ", ((uint8_t *)data)[i]);                            \
+    }                                                                   \
+    logf("%u %lu %u %d\r\n", arg1, arg2, arg3, arg4);                   \
+  }
 
 #define print_bytes(data, len, name) \
   log_bytes(log_debugf, log_debugf, data, len, name, 0, 0, 0, 0)

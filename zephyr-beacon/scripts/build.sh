22258f2e36e849484fcbd70a1343e5bc3eccc571
Aastha Mehta <aasthakm@mpi-sws.org>
2022-05-14 23:14:31 -0700

    update

--- zephyr-beacon/scripts/build.sh
+++ zephyr-beacon/scripts/build.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-rootdir=~/workspace-research/pancast/dev2
-srcdir=$rootdir/pancast-code/zephyr-beacon
-outdir=$srcdir/build
+#######################################
+## build.sh
+##
+##     author: aasthakm
+## created on: May 13, 2022
+##
+## script to compile zephyr beacon code
+#######################################
 
-mkdir -p $outdir
+sys="pancast"
+zdev_str="zephyr-beacon"
+zkit_str="NRF52832_xxAA_REV2"
+
+rootdir=~/workspace-research/pancast/dev2
+srcdir=$rootdir/pancast-code
+z_beacondir=$srcdir/$zdev_str
+outdir="$z_beacondir/build"
+z_beaconimgdir="$z_beacondir/build/zephyr"
+z_beaconimg="$z_beaconimgdir/$sys-$zdev_str.hex"
+
+#mkdir -p $outdir
 cd $rootdir/ncs/zephyr
-west build -c -p auto -b nrf52dk_nrf52832 $srcdir -d $outdir -- -Wno-dev -DCMAKE_EXPORT_COMPILE_COMMANDS=ON > $srcdir/make.out
+west build -c -p auto -b nrf52dk_nrf52832 $z_beacondir  \
+  -d $outdir -- -Wno-dev -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+  > $z_beacondir/make.out
+
+cp $z_beaconimgdir/zephyr.hex "$z_beaconimgdir/$sys-$zdev_str.hex"
